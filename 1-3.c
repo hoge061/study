c558a211ea30045cf66fadcd85091660abc92513
hoge061 <hoge061@gmail.com>
2023-08-04 15:42:49 +0900

    てすと2

---
+++ 1-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void kansu1(){
+    printf("ふるいけや");
+}
+
+void kansu2(){
+    printf("かわずとびこむ");
+}
+
+void kansu3(){
+    printf("みずのおと");
+}
+
+void main(){
+    kansu1();
+    kansu2();
+    kansu3();
+}
